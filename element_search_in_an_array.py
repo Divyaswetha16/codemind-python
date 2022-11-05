f5aec43d28195c2fb052e0d9be370c3545b3d3da
Divyaswetha16 <81951371+Divyaswetha16@users.noreply.github.com>
2022-11-05 11:39:59 +0530

    n=int(input()) v=list(map(int,input().split())) s=int(input()) f=0 for k in v:     if k==s:         print('True')         f=1         break if f==0:     print('False')

---
+++ element_search_in_an_array.py
@@ -0,0 +1,12 @@
+n=int(input())
+v=list(map(int,input().split()))
+s=int(input())
+f=0
+for k in v:
+    if k==s:
+        print('True')
+        f=1
+        break
+if f==0:
+    print('False')
+    
